cb590febfb002ad78f90e6ae0a6bbfe394a0dc4b
Vitor Kloy de Oliveira <146387739+vitorkloy@users.noreply.github.com>
2025-04-17 17:01:24 -0300

    Adicionei README.md, o requirements.txt e atualizei o .gitignore

--- bd.py
+++ bd.py
@@ -1,11 +1,15 @@
 from pymongo import MongoClient
 from CTkMessagebox import CTkMessagebox
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  
 
 def connect():
     try:
-        uri = "mongodb+srv://root:Bb4iuGBz46mB3Ioq@mongodb.hfi0yre.mongodb.net/?retryWrites=true&w=majority&appName=mongoDB"
+        uri = os.getenv("MONGODB_URI") 
         db = MongoClient(uri)
         return db["security_pay"]
     except Exception as e:
         print("erro " + str(e))
-        return CTkMessagebox(title="Erro", icon="warning", message=f"Ocorreu um erro: {str(e)}")  
+        return CTkMessagebox(title="Erro", icon="warning", message=f"Ocorreu um erro: {str(e)}")
